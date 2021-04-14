d2caedf659876a8decd292cdd2d7cddd89c868a3
Jose Mendoza <pball626@gmail.com>
2021-04-14 16:22:56 -0500

    commit tree 8c43fdfd4e58757f8a56e719f2dc0cda90da0499

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2
 
 // Write your code here
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+
+
+let num3 = 24
+let num4 = 10
+let mod = num3 % num4
+
+let max = (1,15,20)
